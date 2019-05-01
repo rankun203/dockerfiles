0252a3b75591e6c611f8fa98cf2cb49d7041045e
Kun Ran <rankun203@gmail.com>
2019-05-01 10:22:59 +0800

    feat: add gosu, install_drupal.sh

---
+++ ubuntu-composer/rootfs/install_drupal.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo 'mkdir app && chown -R www-data:www-data app && cd app'
+mkdir app && chown -R www-data:www-data app && cd app
+
+echo 'gosu www-data git clone https://code.keesee.net/nano/drupal-project.git .'
+gosu www-data git clone https://code.keesee.net/nano/drupal-project.git .
+
+echo 'gosu www-data composer install'
+gosu www-data composer install
+
+echo Composer install successful...
+
+echo Start the server:
+echo ./vendor/bin/drupal rs 0.0.0.0:8080
