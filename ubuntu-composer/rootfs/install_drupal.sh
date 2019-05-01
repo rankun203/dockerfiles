6654b3d0ad900adbf8e52f5108ba572c6b463b1d
Kun Ran <rankun203@gmail.com>
2019-05-01 10:25:28 +0800

    doc: add comments

--- ubuntu-composer/rootfs/install_drupal.sh
+++ ubuntu-composer/rootfs/install_drupal.sh
@@ -7,9 +7,10 @@ echo 'gosu www-data git clone https://code.keesee.net/nano/drupal-project.git .'
 gosu www-data git clone https://code.keesee.net/nano/drupal-project.git .
 
 echo 'gosu www-data composer install'
+echo 'If installation is too slow, switch packagist first:\ncomposer config -g repo.packagist composer https://packagist.laravel-china.org'
 gosu www-data composer install
 
-echo Composer install successful...
+echo 'Composer install successful...'
 
-echo Start the server:
+echo 'Start the server:'
 echo ./vendor/bin/drupal rs 0.0.0.0:8080
